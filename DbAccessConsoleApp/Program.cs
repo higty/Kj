1f238fad33f3e40cd8687d30ea22e8981bfcfadc
higty <higuchihiguchihiguchi@hotmail.com>
2020-03-05 11:10:20 +0900

    プロジェクト ファイルを追加します。

---
+++ DbAccessConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DbAccessConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
