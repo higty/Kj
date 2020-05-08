393daa6d6bd027a80f4af0b6917c0284e37889c4
higty <higuchihiguchihiguchi@hotmail.com>
2020-05-08 19:20:39 +0900

    Study 20200508.

---
+++ DbAccessConsoleApp/MyMethodList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DbAccessConsoleApp
+{
+    class MyMethodList
+    {
+        public static void Method_Int32Plus()
+        {
+            Console.WriteLine("1個目の数字");
+            Int32 x1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("2個目の数字");
+            Int32 x2 = Int32.Parse(Console.ReadLine());
+            Int32 x3 = x1 + x2;
+            Console.WriteLine(x3.ToString());
+            Console.ReadLine();
+        }
+        public static void Method_StringJoin()
+        {
+            Console.WriteLine("1個目の文字列");
+            String c1 = Console.ReadLine();
+            Console.WriteLine("2個目の文字列");
+            String c2 = Console.ReadLine();
+
+            Console.WriteLine(c1 + c2);
+            Console.ReadLine();
+        }
+        public static void Method_Japanese()
+        {
+            Console.WriteLine("何か入力してください。");
+            String line = Console.ReadLine();
+            Console.WriteLine("あなたの入力＝" + line);
+
+            Console.ReadLine();
+        }
+        public static void Method_English()
+        {
+            Console.WriteLine("Please input...");
+            String line = Console.ReadLine();
+            Console.WriteLine("Your input=" + line);
+
+            Console.ReadLine();
+        }
+    }
+}
--- DbAccessConsoleApp/Program.cs
+++ DbAccessConsoleApp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Reflection.Metadata;
+using System.Text;
+using System.IO;
 
 namespace DbAccessConsoleApp
 {
@@ -6,7 +9,28 @@ namespace DbAccessConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        }
+        static void File_Read()
+        {
+            String filePath = "C:\\Dev\\MyTextFile.txt";
+            String body = System.IO.File.ReadAllText(filePath);
+            Console.WriteLine(body);
+            Console.ReadLine();
+        }
+        static void File_Write()
+        {
+            String filePath = "C:\\Dev\\MyTextFile.txt";
+            File.WriteAllText(filePath, "中身を変えたよ！");
+            Console.ReadLine();
+        }
+        static void File_Encoding()
+        {
+            var encoding = CodePagesEncodingProvider.Instance.GetEncoding(932);
+            String filePath = "C:\\Dev\\MyTextFile.txt";
+            String body = System.IO.File.ReadAllText(filePath, encoding);
+            Console.WriteLine(body);
+            File.WriteAllText(filePath, "中身を変えたよ！");
+            Console.ReadLine();
         }
     }
 }
