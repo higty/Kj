030dc57032860a29c218e67a80f778a1b0b5e4e9
higty <higuchihiguchihiguchi@hotmail.com>
2020-05-14 19:31:46 +0900

    Commit for Study 20200514.

--- DbAccessConsoleApp/Program.cs
+++ DbAccessConsoleApp/Program.cs
@@ -2,6 +2,8 @@
 using System.Reflection.Metadata;
 using System.Text;
 using System.IO;
+using System.Data;
+using System.Data.SqlClient;//MSのSQLサーバーへのアクセス
 
 namespace DbAccessConsoleApp
 {
@@ -9,7 +11,50 @@ namespace DbAccessConsoleApp
     {
         static void Main(string[] args)
         {
-            MyMethodList.Method_Int32Plus();
+        }
+        static void DB_DTask_Insert()
+        {
+            var connectionString = File.ReadAllText("C:\\GitHub\\ConnectionString.txt");
+            Console.WriteLine(connectionString);
+
+            //接続文字列のDBへ接続するためのSqlConnectionクラスを作成してcnで利用。
+            using (var cn = new SqlConnection(connectionString))
+            {
+                //SQLを設定するSqlCommandクラスを作成
+                var cm = new SqlCommand("insert into DTask values(NEWID(), 'SQLの復習2', GETDATE(), '松田', '2020-5-24', '寺子屋1')");
+                //SQLを実行する先のDBを設定する
+                cm.Connection = cn;
+                //接続を開く
+                cn.Open();
+                //実際にSQLを実行する
+                var insertCount = cm.ExecuteNonQuery();
+                //接続を閉じる
+                cn.Close();
+            }
+            Console.ReadLine();
+        }
+        static void DB_DTask_RecordCount_Get()
+        {
+            var connectionString = File.ReadAllText("C:\\GitHub\\ConnectionString.txt");
+            Console.WriteLine(connectionString);
+
+            //接続文字列のDBへ接続するためのSqlConnectionクラスを作成してcnで利用。
+            using (var cn = new SqlConnection(connectionString))
+            {
+                //SQLを設定するSqlCommandクラスを作成
+                var cm = new SqlCommand("select COUNT(*) from DTask");
+                //SQLを実行する先のDBを設定する
+                cm.Connection = cn;
+                //接続を開く
+                cn.Open();
+                //実際にSQLを実行する
+                var o = cm.ExecuteScalar();
+                Console.WriteLine(o);
+                //接続を閉じる
+                cn.Close();
+            }
+            Console.ReadLine();
+
         }
         static void File_Read()
         {
