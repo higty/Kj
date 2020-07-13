1c9e8cffcae4d3b3b6a29ed76d190d1b4628908f
higty <higuchihiguchihiguchi@hotmail.com>
2020-07-13 19:51:36 +0900

    2020/07/13 items. Property. Auto property. Constructor.

--- ClassStudyConsoleApp/Person.cs
+++ ClassStudyConsoleApp/Person.cs
@@ -9,20 +9,46 @@ namespace ClassStudyConsoleApp
     /// 
     /// ■クラスのメリットその１
     /// 複数の値をグループ化できる
-    /// 
+    /// 必ずInitializeメソッドを呼ぶこと！！！
     /// 
     /// </summary>
     class Person
     {
-        public String Name = "";
-        public String HomeTown = "";
-        public String Number = "";
-        public String OrganizationName = "";
-        public String PositionName = "";
+        private Int32 _Age = 0;
 
+        public String Name { get; set; }
+        public String HomeTown { get; set; }
+        public String Number { get; set; }
+        public String OrganizationName { get; set; }
+        public String PositionName { get; set; }
+        public Int32 Age
+        {
+            get { return _Age; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException();
+                }
+                _Age = value;
+            }
+        }
+
+        public Person(String name, Int32 age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public void Initialize(String name, Int32 age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
         public void ShowName()
         {
-            Console.WriteLine("名前は" + this.Name + "です。");
+            Console.WriteLine("名前は" + this.Name + "です。"
+                + this.Age + "歳です。");
         }
     }
 }
--- ClassStudyConsoleApp/Program.cs
+++ ClassStudyConsoleApp/Program.cs
@@ -11,34 +11,40 @@ namespace ClassStudyConsoleApp
             //フィールド
             //インスタンス
             //メソッド
-            //コンストラクタ
+            //アクセッサ―(public, private, protected, internal)
+            //private クラスの外側からアクセスができないようにする。
             //プロパティ
-            //アクセッサ―
+            //自動プロパティ（フィールド＆プロパティのセットを簡単に書く方法
+            //コンストラクタ
             //継承
             //
             //コンピューター。HD、メモリ、CPU、画面
-           
+
             //入力が簡単になる。（IDEのおかげ）
             //使用する項目をグループ化できる。
-            var p = new Person();
-            p.Name = "Hig";
+            var p = new Person("Hig", 40);
             p.HomeTown = "福岡";
             p.Number = "1234";
             p.OrganizationName = "機械部";
             p.PositionName = "課長";
             p.ShowName();
+
+            var higAge = p.Age;
             //Console.WriteLine(p.Name + "です。" + p.HomeTown + "生まれです。");
 
-            var p1 = new Person();
+            var p1 = new Person("Komii", 36);
             p1.Name = "Komii";
             p1.HomeTown = "北海道";
             p1.Number = "3306";
             p1.OrganizationName = "機械部";
             p1.PositionName = "部長";
+            p1.Age = 10;
             p1.ShowName();
 
 
             Console.ReadLine();
         }
+
+        static void MyMethod() { }
     }
 }
