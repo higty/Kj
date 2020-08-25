94903f9213acfbad651fc986ea766d4f7adcc426
higty <higuchihiguchihiguchi@hotmail.com>
2020-08-25 17:38:45 +0900

    Add DbAccessWpfApp2 project.

---
+++ DbAccessWpfApp2/Database.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace DbAccessWpfApp2
+{
+    public class Database
+    {
+        public String ConnectionString { get; set; }
+
+        public void ExecuteNonQuery(String query)
+        {
+            using (var cn = new SqlConnection(this.ConnectionString))
+            {
+                var cm = new SqlCommand(query);
+                cm.Connection = cn;
+                cn.Open();
+                var o = cm.ExecuteNonQuery();
+                cn.Close();
+            }
+        }
+    }
+}
---
+++ DbAccessWpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DbAccessWpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 保守性
+        /// 可読性
+        /// 堅牢性
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var connectionString = File.ReadAllText("C:\\Study\\ConnectionString_Kj_Wpf.txt");
+
+            var query1 = String.Format("insert into MDumpCar(CarCD,DisplayName,CreateTime,LoadCapacity,ProductCode,Price)"
+                + "values(NEWID(), '{0}', GETDATE(), {1}, '{2}', {3})"
+                , this.DisplayNameTextbox.Text, this.LoadCapacityTextbox.Text, this.ProductCodeTextbox.Text, this.PriceTextbox.Text);
+
+            var db = new Database();
+            db.ConnectionString = connectionString;
+            db.ExecuteNonQuery(query1);
+
+            MessageBox.Show("追加しました！");
+        }
+    }
+}
