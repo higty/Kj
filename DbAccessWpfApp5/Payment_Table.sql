2fb741c31c0cb16cec0080f99a2ce9ed0da6a1af
higty <higuchihiguchihiguchi@hotmail.com>
2020-10-19 18:25:00 +0900

    データの退避。 テーブルの作り直し。 データの確認。

--- DbAccessWpfApp5/Payment_Table.sql
+++ DbAccessWpfApp5/Payment_Table.sql
@@ -1,10 +1,25 @@
-﻿CREATE TABLE Payment
+﻿--新しくデータを作成してコピーする
+select * into Payment_20201019 from Payment
+
+--新しく作ったテーブルのデータを確認する
+select * from Payment_20201019
+
+Drop Table Payment
+Go
+
+CREATE TABLE Payment
 (PaymentCD uniqueidentifier NOT NULL
 ,Title nvarchar(100) NOT NULL
 ,Date date NOT NULL
 ,Price int NOT NULL
 ,Constraint Payment_PrimaryKey Primary Key NonClustered(PaymentCD)
-
 ) 
 GO
+
+select * from Payment
+
+insert into Payment(PaymentCD,Title,Date,Price)
+select PaymentCD,Title,Date,Price from Payment_20201019
+
+select * from Payment
 
