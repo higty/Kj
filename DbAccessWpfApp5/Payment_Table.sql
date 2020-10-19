ab7bfe9f418d88c3b1210c4f09368dd9ea007f67
higty <higuchihiguchihiguchi@hotmail.com>
2020-10-19 18:00:30 +0900

    Paymentテーブルを作るスクリプトファイルを追加。

---
+++ DbAccessWpfApp5/Payment_Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE Payment
+(PaymentCD uniqueidentifier NOT NULL
+,Title nvarchar(100) NOT NULL
+,Date date NOT NULL
+,Price int NOT NULL
+,Constraint Payment_PrimaryKey Primary Key NonClustered(PaymentCD)
+
+) 
+GO
+
