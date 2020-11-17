84fb7b0c7585aba385d88cfb94653bbd4a5fdde4
higty <higuchihiguchihiguchi@hotmail.com>
2020-11-17 19:09:51 +0900

    Commit.

---
+++ DbAccessWpfApp6/Back4Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DbAccessWpfApp6
+{
+    public class BackHoeCar : Car
+    {
+        public BackHoeCar(String name)
+        {
+            this.Name = name;
+        }
+    }
+}
--- DbAccessWpfApp6/Program.cs
+++ DbAccessWpfApp6/Program.cs
@@ -4,20 +4,32 @@ namespace DbAccessWpfApp6
 {
     class Program
     {
+        /// <summary>
+        /// プロパティ、メソッド、イベントなどを子クラスで再利用できる
+        /// 親クラスの変数に子クラスのインスタンスを代入できる
+        /// virtaul method(仮想メソッド)は子クラスでoverrideできる許可を付与する
+        /// overrideすると変数に代入したインスタンスのクラスのメソッドの方を呼べる
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            var carList = new Car[3];
+            var carList = new Car[4];
 
             carList[0] = new DumpCar("ダンプ1");
             carList[1] = new Bulldozer("ブルドーザー2");
             carList[2] = new Roller("ローラー3");
+            carList[3] = new BackHoeCar("バックフォー");
 
             for (int i = 0; i < carList.Length; i++)
             {
+                Console.WriteLine("名前は" + carList[i].Name);
                 Console.WriteLine(carList[i].GetPicture());
+                Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
             }
             Console.ReadLine();
 
+            //「リファクタリング」をしましょう。
+            //デザインパターン
         }
         private static void Parent_Child_Sample()
         {
