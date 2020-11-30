0ce4f4c47b2f1b74d0119d84ef747f227ef09385
higty <higuchihiguchihiguchi@hotmail.com>
2020-11-30 20:20:32 +0900

    Commit.

--- WebScrapingApp/Program.cs
+++ WebScrapingApp/Program.cs
@@ -8,6 +8,38 @@ namespace WebScrapingApp
     {
         static void Main(string[] args)
         {
+        }
+        private static void Scrape_Html()
+        {
+            var html = @"<html>
+  <body>
+    <h1>ワンピース</h1>
+    <div class=""item"">
+      <span class=""brand"">iQON</span>
+      <span class=""regular_price"">1,200円</span>
+      <span class=""sale_price"">1,000円</span>
+    </div>
+    <span class=""title-1"">10月</span>
+    <span class=""title-2"">11月</span>
+    <span class=""title-3"">12月</span>
+  </body>
+</html>";
+
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            var nodes = doc.DocumentNode.SelectNodes("//div/span[contains(@class, 'title')]");
+            foreach (HtmlNode node in nodes)
+            {
+                Console.WriteLine(node.InnerText);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
+        }
+        private static void Scrape_YahooNews()
+        {
             var cl = new HttpClient();
             //非同期呼び出し
             var html = cl.GetStringAsync("https://news.yahoo.co.jp/topics/entertainment").Result;
@@ -28,6 +60,7 @@ namespace WebScrapingApp
             Console.WriteLine();
             Console.WriteLine("Press Enter to exit");
             Console.ReadLine();
+
         }
     }
 }
