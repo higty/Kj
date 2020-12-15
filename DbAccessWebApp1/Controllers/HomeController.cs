0cdb82a7afdc981dbd259a215a08cec6ba862a7d
higty <higuchihiguchihiguchi@hotmail.com>
2020-12-16 08:13:21 +0900

    Add DbAccessWebApp1 project.

---
+++ DbAccessWebApp1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using DbAccessWebApp1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DbAccessWebApp1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var page = new IndexPage();
+            page.Now = DateTime.Now;
+            return View("Index", page);
+        }
+        public class IndexPage
+        {
+            public DateTime Now { get; set; }
+            public String SomeValue { get; set; }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
