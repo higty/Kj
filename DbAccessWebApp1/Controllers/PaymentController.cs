fb794ac8c64b89513a242433767a7668b1a7477e
higty <higuchihiguchihiguchi@hotmail.com>
2020-12-21 19:01:10 +0900

    Fix /Payment/List page.

--- DbAccessWebApp1/Controllers/PaymentController.cs
+++ DbAccessWebApp1/Controllers/PaymentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DbAccessDatabase;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,24 @@ namespace DbAccessWebApp1.Controllers
 {
     public class PaymentController : Controller
     {
+        public class ListPage
+        {
+            public String Text { get; set; }
+            public List<PaymentRecord> PaymentList { get; set; } = new List<PaymentRecord>();
+        }
         public IActionResult List()
         {
-            return View();
+            var model = new ListPage();
+            model.Text = "今年もあと10日！";
+
+            for (int i = 0; i < 10; i++)
+            {
+                var r = new PaymentRecord();
+                r.Title = "商品" + i;
+                r.Price = i * 100;
+                model.PaymentList.Add(r);
+            }
+            return View(model);
         }
     }
 }
--- DbAccessWebApp1/Startup.cs
+++ DbAccessWebApp1/Startup.cs
@@ -54,7 +54,7 @@ namespace DbAccessWebApp1
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Payment}/{action=List}/{id?}");
             });
         }
     }
