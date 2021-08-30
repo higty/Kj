f0639fc7d5a653d91c7eaa436c03a2bea26a8be9
higty <higuchihiguchihiguchi@hotmail.com>
2021-08-30 18:39:21 +0900

    Add Database class.

---
+++ CSharpDatabase/Database.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace CSharpDatabase
+{
+    public class Database
+    {
+        public String ConnectionString { get; init; } = "";
+
+        public Database(String connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+   
+        public Int32 ExecuteNonQuery(String query)
+        {
+            using (var cn = new SqlConnection(this.ConnectionString))
+            {
+                var cm = new SqlCommand(query);
+                cm.Connection = cn;
+                cn.Open();
+                var affectedRecordCount = cm.ExecuteNonQuery();
+                cn.Close();
+                return affectedRecordCount;
+            }
+        }
+        public Int32 ExecuteNonQuery(SqlCommand command)
+        {
+            var cm = command;
+
+            using (var cn = new SqlConnection(this.ConnectionString))
+            {
+                cm.Connection = cn;
+                cn.Open();
+                var affectedRecordCount = cm.ExecuteNonQuery();
+                cn.Close();
+                return affectedRecordCount;
+            }
+        }
+        public async Task<Int32> ExecuteNonQueryAsync(String query)
+        {
+            using (var cn = new SqlConnection(this.ConnectionString))
+            {
+                var cm = new SqlCommand(query);
+                cm.Connection = cn;
+                cn.Open();
+                var affectedRecordCount = await cm.ExecuteNonQueryAsync();
+                cn.Close();
+                return affectedRecordCount;
+            }
+        }
+        public async Task<Int32> ExecuteNonQueryAsync(SqlCommand command)
+        {
+            var cm = command;
+
+            using (var cn = new SqlConnection(this.ConnectionString))
+            {
+                cm.Connection = cn;
+                cn.Open();
+                var affectedRecordCount = await cm.ExecuteNonQueryAsync();
+                cn.Close();
+                return affectedRecordCount;
+            }
+        }
+    }
+}
---
+++ CSharpDatabaseConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+using System.IO;
+
+namespace CSharpDatabase 
+{
+    class Program
+    {
+        public static readonly String ConnectionString = File.ReadAllText("C:\\GitHub\\ConnectionString.txt");
+
+        static void Main(string[] args)
+        {
+            ExecuteInsertCommand();
+
+            Console.WriteLine("Press enter exit...");
+            Console.ReadLine();
+        }
+        private static void ExecuteInsertQuery()
+        {
+            var db = new Database(Program.ConnectionString);
+            db.ExecuteNonQuery("insert into Payment values(NEWID(), '雪見大福', '2021-08-30', 160)");
+        }
+        private static void ExecuteInsertCommand()
+        {
+            var db = new Database(Program.ConnectionString);
+            var cm = CreateInsertCommand();
+            db.ExecuteNonQuery(cm);
+        }
+        private static SqlCommand CreateInsertCommand()
+        {
+            var cm = new SqlCommand("insert into Payment(PaymentCD,Title,Date,Price) values(NEWID(), '@Title', '@Date', @Price)");
+
+            cm.Parameters.Add(new SqlParameter("@Title", SqlDbType.NVarChar, 100) { Value = "チョコポッキー" });
+            cm.Parameters.Add(new SqlParameter("@Date", SqlDbType.Date) { Value = "2021-08-31" });
+            cm.Parameters.Add(new SqlParameter("@Price", SqlDbType.Int) { Value = "210" });
+
+            return cm;
+        }
+    }
+}
