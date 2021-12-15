f9ccff4f8d007fcc1ad78d632276b5071a8d4eaf
higty <higuchihiguchihiguchi@hotmail.com>
2021-12-15 17:30:56 +0900

    Add Test case. Fix csv file name.

--- CsvLibraryTest/UnitTest1.cs
+++ CsvLibraryTest/UnitTest1.cs
@@ -44,7 +44,7 @@ namespace CsvLibraryTest
         {
             var reader = new CsvReader();
             //var s = "“c’†,21,“Œ‹“s`‹æ" + Environment.NewLine + "—é–Ø,22,“Œ‹“sVh‹æ";           
-            var filePath = System.IO.Path.Combine(Environment.CurrentDirectory, "Csv", "CsvSample _DoubleQuatation.csv");
+            var filePath = System.IO.Path.Combine(Environment.CurrentDirectory, "Csv", "CsvSample_DoubleQuatation.csv");
             var bodyText = File.ReadAllText(filePath);
             var rowList = reader.Read(bodyText);
 
@@ -53,6 +53,28 @@ namespace CsvLibraryTest
             Assert.Equal("“c’†", line0[0]);
             Assert.Equal("21", line0[1]);
             Assert.Equal("“Œ‹“s`‹æ", line0[2]);
+
+            var line1 = rowList[1];
+            Assert.Equal(3, line1.Length);
+            Assert.Equal("—é–Ø", line1[0]);
+            Assert.Equal("22", line1[1]);
+            Assert.Equal("“Œ‹“sVh‹æ", line1[2]);
+        }
+        [Fact]
+        public void Test_FromFile_DoubleQuatation_WithNewLine()
+        {
+
+            var reader = new CsvReader();
+            //var s = "“c’†,21,“Œ‹“s`‹æ" + Environment.NewLine + "—é–Ø,22,“Œ‹“sVh‹æ";           
+            var filePath = System.IO.Path.Combine(Environment.CurrentDirectory, "Csv", "CsvSample_DoubleQuatation_WithNewLine.csv");
+            var bodyText = File.ReadAllText(filePath);
+            var rowList = reader.Read(bodyText);
+
+            var line0 = rowList[0];
+            Assert.Equal(3, line0.Length);
+            Assert.Equal("“c’†", line0[0]);
+            Assert.Equal("21", line0[1]);
+            Assert.Equal("“Œ‹“s\r\n`‹æ", line0[2]);
 
             var line1 = rowList[1];
             Assert.Equal(3, line1.Length);
