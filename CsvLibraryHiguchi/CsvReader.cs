3d049aff902ef64a6a98719a7df210a38beb28cc
higty <higuchihiguchihiguchi@hotmail.com>
2022-01-13 16:46:43 +0900

    Add test case of double quatation inside double quatation.

---
+++ CsvLibraryHiguchi/CsvReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CsvLibrary
+{
+    public class CsvReader
+    {
+        public List<String[]> Read(String bodyText)
+        {
+            var rowList = new List<String[]>();
+
+            return rowList;
+        }
+    }
+}
